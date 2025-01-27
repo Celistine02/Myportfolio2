d783c69b7e845419a2f5be9880ba3842302c9e19
Celistine02 <148200763+Celistine02@users.noreply.github.com>
2025-01-27 12:22:39 +0200

    Update Testimonial.js

--- src/components/tesimonial/Testimonial.js
+++ src/components/tesimonial/Testimonial.js
@@ -1,10 +1,9 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react';
 import Slider from "react-slick";
 import { RiStarFill } from "react-icons/ri";
 import { HiArrowRight, HiArrowLeft } from "react-icons/hi";
-import Title from '../layouts/Title'
-import { testimonialOne,testimonialTwo, quote } from "../../assets";
-
+import Title from '../layouts/Title';
+import { testimonialOne, testimonialTwo, quote } from "../../assets";
 
 function SampleNextArrow(props) {
   const { onClick } = props;
@@ -31,62 +30,50 @@ function SamplePrevArrow(props) {
 }
 
 const Testimonial = () => {
-      const [dotActive, setDocActive] = useState(0);
-     const settings = {
-       dots: true,
-       infinite: true,
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       nextArrow:<SampleNextArrow />,
-       prevArrow:<SamplePrevArrow />,
-       beforeChange: (prev, next) => {
-         setDocActive(next);
-       },
-       appendDots: (dots) => (
-         <div
-           style={{
-             borderRadius: "10px",
-             padding: "10px",
-           }}
-         >
-           <ul
-             style={{
-               display: "flex",
-               gap: "15px",
-               justifyContent: "center",
-               marginTop: "20px",
-             }}
-           >
-             {" "}
-             {dots}{" "}
-           </ul>
-         </div>
-       ),
-       customPaging: (i) => (
-         <div
-           style={
-             i === dotActive
-               ? {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "#ff014f",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-               : {
-                   width: "12px",
-                   height: "12px",
-                   color: "blue",
-                   background: "gray",
-                   borderRadius: "50%",
-                   cursor: "pointer",
-                 }
-           }
-         ></div>
-       ),
-     };
+  const [dotActive, setDocActive] = useState(0);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    beforeChange: (prev, next) => {
+      setDocActive(next);
+    },
+    appendDots: (dots) => (
+      <div
+        style={{
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        <ul
+          style={{
+            display: "flex",
+            gap: "15px",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
+        >
+          {dots}
+        </ul>
+      </div>
+    ),
+    customPaging: (i) => (
+      <div
+        style={{
+          width: "12px",
+          height: "12px",
+          background: i === dotActive ? "#ff014f" : "gray",
+          borderRadius: "50%",
+          cursor: "pointer",
+        }}
+      />
+    ),
+  };
+
   return (
     <section
       id="testimonial"
@@ -96,7 +83,6 @@ const Testimonial = () => {
         <Title title="WHAT CLIENTS SAY" des="Testimonial" />
       </div>
       <div className="max-w-6xl mx-auto">
-        {/* ================ Slider One ================== */}
         <Slider {...settings}>
           <div className="w-full">
             <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
@@ -107,9 +93,6 @@ const Testimonial = () => {
                   alt="testimonialOne"
                 />
                 <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-            
-                  </p>
                   <h3 className="text-2xl font-bold">Timothy Musenga</h3>
                   <p className="text-base tracking-wide text-gray-500">
                     CEO & Co-Founder 
@@ -120,14 +103,12 @@ const Testimonial = () => {
                 <img className="w-20 lgl:w-32" src={quote} alt="quote" />
                 <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
-                    <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Remote Device Locking.
-                      </h3>
-                      <p className="text-base text-gray-400 mt-3">
-                        via Upwork - Nov, 2024 - Jan 22, 2025 test
-                      </p>
-                    </div>
+                    <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      Remote Device Locking.
+                    </h3>
+                    <p className="text-base text-gray-400 mt-3">
+                      via Upwork - Nov, 2024 - Jan 22, 2025 test
+                    </p>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
                       <RiStarFill />
@@ -143,8 +124,6 @@ const Testimonial = () => {
               </div>
             </div>
           </div>
-          {/* ================ Slider Two ================== */}
-
           <div className="w-full">
             <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
               <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
@@ -154,9 +133,6 @@ const Testimonial = () => {
                   alt="testimonialTwo"
                 />
                 <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    
-                  </p>
                   <h3 className="text-2xl font-bold">Joshua Smith</h3>
                   <p className="text-base tracking-wide text-gray-500">
                     Principal at Muzarabani High
@@ -167,14 +143,12 @@ const Testimonial = () => {
                 <img className="w-20 lgl:w-32" src={quote} alt="quote" />
                 <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
                   <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
-                    <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Smart School management system
-                      </h3>
-                      <p className="text-base text-gray-400 mt-3">
-                        via Upwork - September 4, 2024 - Jan 19, 2024 test
-                      </p>
-                    </div>
+                    <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
+                      Smart School management system
+                    </h3>
+                    <p className="text-base text-gray-400 mt-3">
+                      via Upwork - September 4, 2024 - Jan 19, 2024 test
+                    </p>
                     <div className="text-yellow-500 flex gap-1">
                       <RiStarFill />
                       <RiStarFill />
@@ -190,61 +164,10 @@ const Testimonial = () => {
               </div>
             </div>
           </div>
-          {/* ================ Slider Three ================== */}
-
-          <div className="w-full">
-            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
-              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
-                <img
-                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
-                  src={testimonialOne}
-                  alt="testimonialOne"
-                />
-                <div className="w-full flex flex-col justify-end">
-                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
-                    Bound - Trolola
-                  </p>
-                  <h3 className="text-2xl font-bold">Jone Duone Joe</h3>
-                  <p className="text-base tracking-wide text-gray-500">
-                    Operation Officer
-                  </p>
-                </div>
-              </div>
-              <div className="w-full lgl:w-[60%] h-full flex flex-col justify-between">
-                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
-                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
-                  <div className="flex flex-col justify-between lgl:items-center py-6 border-b-2 border-b-gray-900">
-                    <div>
-                      <h3 className="text-xl lgl:text-2xl font-medium tracking-wide">
-                        Travel Mobile App Design.
-                      </h3>
-                      <p className="text-base text-gray-400 mt-3">
-                        via Upwork - Mar 4, 2015 - Aug 30, 2021 test
-                      </p>
-                    </div>
-                    <div className="text-yellow-500 flex gap-1">
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                      <RiStarFill />
-                    </div>
-                  </div>
-                  <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-                    dolorum, eos natus ipsum numquam veniam officia
-                    necessitatibus ratione quos debitis exercitationem
-                    repudiandae facilis id neque nihil accusantium perspiciatis
-                    repellat? Iste.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
         </Slider>
       </div>
     </section>
   );
 }
 
-export default Testimonial
+export default Testimonial;
