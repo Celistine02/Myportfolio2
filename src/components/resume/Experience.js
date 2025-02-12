ed147f1148ec325330013dd02493244332947994
Celistine02 <148200763+Celistine02@users.noreply.github.com>
2025-02-12 11:21:36 +0200

    Update Experience.js

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -42,23 +42,13 @@ const Experience = () => {
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Gym Instructor"
-            subTitle="Rainbow Gym Center (2015 - 2020)"
-            result="DHAKA"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+            title="Computer Science and Mathematics Teacher"
+            subTitle="Kutenda College High School (2019 - 2022)"
+            result="Zimbabwe"
+            des="Teaching Computer Science and Mathematics to students, preparing them for future careers in the technology industry."
           />
-          <ResumeCard
-            title="Web Developer and Instructor"
-            subTitle="SuperKing College (2010 - 2014)"
-            result="CANADA"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
-          <ResumeCard
-            title="School Teacher"
-            subTitle="Kingstar Secondary School (2001 - 2010)"
-            result="NEVADA"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          />
+          
+          
         </div>
       </div>
     </motion.div>
